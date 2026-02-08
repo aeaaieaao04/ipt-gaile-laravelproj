9a0244b0e9e387ebacc4667fabf1783ab425cf13
Angela Gaile Artajo <angelaartajo04@gmail.com>
2026-02-08 23:58:22 +0800

    UserContoller Added

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/user', function (Request $request) {
     echo "Gaile's Laravel Project";
+});
+
+Route::controller(UserController::class)->prefix('users')->group(function () {
+    Route::get('/', 'index');
+    Route::post('/', 'store');
+    Route::get('/{id}', 'show');
+    Route::put('/{id}', 'update');
+    Route::delete('/{id}', 'destroy');
 });
